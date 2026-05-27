04b38b3453aed95f8cc795b033930bbb049b6afa
GirinandiniSenthilkumar <sgirinandini333@gmail.com>
2026-05-27 14:22:18 +0530

    feat: Add Replace Employee ID With The Unique Identifier

--- Database/Replace Employee ID With The Unique Identifier/sql/solution.sql
+++ Database/Replace Employee ID With The Unique Identifier/sql/solution.sql
@@ -1,5 +1,4 @@
 # Write your MySQL query statement below
 select eu.unique_id,e.name
-from Employees e
-left join EmployeeUNI eu
-on e.id=eu.id
+from Employees e 
+left join EmployeeUNI eu on e.id=eu. id;
