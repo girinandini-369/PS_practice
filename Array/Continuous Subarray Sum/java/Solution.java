d4a9a7c92723e8da04f938c3bcbb216fc2f0ecac
GirinandiniSenthilkumar <sgirinandini333@gmail.com>
2026-06-09 16:57:17 +0530

    feat: Add Continuous Subarray Sum

---
+++ Array/Continuous Subarray Sum/java/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean checkSubarraySum(int[] arr, int k) {
+        boolean ans=false;
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            int sum=0;
+            int len=1;
+            for(int j=i;j<n;j++)
+            {
+              sum=sum+arr[j];
+             // if(sum==k)
+              //{
+                if(len>=2)
+                {
+                    if(sum%k==0)
+                    {
+                        return true;
+                    }
+                }
+             // }
+              len++;
+            }
+        }
+        return ans;
+    }
+}
